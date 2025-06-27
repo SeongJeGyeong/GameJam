831c96227d70f1686300b81e0e36955a0a5542c3
SeongJeGyeong <cmjegyeong@gmail.com>
2025-06-27 17:55:26 +0900

    Create SpineAnimatorBehaviour.cs

---
+++ Assets/Animations/SpineAnimatorBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Spine.Unity;
+public class SpineAnimatorBehaviour : StateMachineBehaviour
+{
+    [SerializeField]
+    AnimationClip motion;
+    string animationClip;
+    [Header("스파인 모션 레이어")]
+    public int layer = 0;
+    public float timeScale = 1f;
+
+    private SkeletonAnimation skeletonAnimation;
+    private Spine.AnimationState spineAnimationState;
+    private Spine.TrackEntry trackEntry;
+    private void Awake()
+    {
+        if (motion != null)
+        {
+            animationClip = motion.name;
+        }
+    }
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (skeletonAnimation == null)
+        {
+            skeletonAnimation = animator.GetComponentInChildren<SkeletonAnimation>();
+            spineAnimationState = skeletonAnimation.state;
+        }
+
+        if (animationClip != null)
+        {
+            trackEntry = spineAnimationState.SetAnimation(layer, animationClip, stateInfo.loop);
+            trackEntry.TimeScale = timeScale;
+        }
+    }
+}
