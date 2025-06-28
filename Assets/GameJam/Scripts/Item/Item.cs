0903f01ac30aaa37675968d48ad83a1f8d4f306f
juaxxxx <164902625+juaxxxx@users.noreply.github.com>
2025-06-28 10:52:15 +0900

    Item 관련 스크립트, 프리팹

--- Assets/GameJam/Scripts/Item/Item.cs
+++ Assets/GameJam/Scripts/Item/Item.cs
@@ -9,10 +9,12 @@ public enum ItemType
     Consumption
 }
 
-public abstract class Item : MonoBehaviour
+public class Item : MonoBehaviour
 {
     [SerializeField]
     public ItemType itemType;
+    [SerializeField]
+    private int itemNumber; // 해당 아이템 번호. 아이템 부위 + itemNuber.Tostring 으로 사용할 거 같아서
 
-    public abstract void Use();
+    public int GetItemNumber() { return itemNumber; }
 }
