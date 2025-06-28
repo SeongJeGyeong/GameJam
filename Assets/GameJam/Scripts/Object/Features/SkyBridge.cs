59c34ccb2c8df64af27023d04e713d92bf6968d5
juaxxxx <164902625+juaxxxx@users.noreply.github.com>
2025-06-28 17:05:25 +0900

    지형 오브젝트 구현, 랜덤상자 초안 작성, 맵 디자인 진행

---
+++ Assets/GameJam/Scripts/Object/Features/SkyBridge.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBridge : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 startPos;
+    [SerializeField]
+    Vector3 endPos;
+    [SerializeField]
+    float speed;
+    Vector3 desPos;
+
+
+    void Start()
+    {
+        transform.position = startPos;
+        desPos = endPos;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, desPos, Time.deltaTime * speed);
+        if (Vector2.Distance(transform.position, desPos) < 0.05f)
+        {
+            if (desPos == endPos) desPos = startPos;
+            else desPos = endPos;
+        }
+    }
+}
